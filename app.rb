422539ee561ab4216e2348b1b526abb5de6034de
Devin Sweeting and Noah Schooler <noah@>
2018-11-26 14:36:36 -0800

    add user interface using Sinatra

--- app.rb
+++ app.rb
@@ -2,3 +2,20 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/parcels.rb')
 require('pry')
+
+get ('/') do
+  erb(:input)
+end
+
+get ('/output') do
+  @length = params['length'].to_i
+  @width = params['width'].to_i
+  @height = params['height'].to_i
+  @weight = params['weight'].to_i
+  @speed = params['speed']
+  parcel = Parcels.new(@length, @width, @height, @weight, @speed)
+  binding.pry
+  @volume = parcel.volume()
+  @cost_to_ship = parcel.cost_to_ship()
+  erb(:output)
+end
